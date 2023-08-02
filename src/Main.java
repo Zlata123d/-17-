f93644e47b4b891a33654ad639c43ffa3fcb25bf
123 <zlata18_00@mail.ru>
2023-08-02 20:05:33 +0300

    Задача1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int  a=10;
+        System.out.println(a);
+        byte b=100;
+        System.out.println(b);
+        short c=30000;
+        System.out.println(c);
+        long d=8000L;
+        System.out.println(d);
+        float e = 3.75f;
+        System.out.println(e);
+        double f=56.78;
+        System.out.println(f);
+
+        }
+    }
